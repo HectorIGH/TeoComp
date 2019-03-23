b69f239856b4814e48173ede9169ded105eeaeba
HectorIGH <47642123+HectorIGH@users.noreply.github.com>
2019-03-23 16:02:39 -0600

    Files and Images added
    Several test .afd files added. User manua improved and Screenshots of the executions added.

--- DFAtoRegEx/DeterministicFiniteAutomaton_To_RegEx.py
+++ DFAtoRegEx/DeterministicFiniteAutomaton_To_RegEx.py
@@ -3,13 +3,30 @@ import numpy as np
 from tkinter.filedialog import askopenfilename
 from tkinter import filedialog
 from tkinter import *
+import os
+import csv
 
 # Reading file removing extra notation
 root = Tk()
 root.withdraw()
-root.filename = filedialog.askopenfilename(initialdir = "/", title = "Select .fda file to extract RegEx", filetypes = ((".fda files", "*.afd"),("all files", "*.*")))
-data = pd.read_csv(root.filename, sep = "\t|,| |=|\)|\(|\{|\}|F.", header = None, engine = 'python')
+root.filename = filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select .fda file to extract RegEx", filetypes = ((".fda files", "*.afd"),("all files", "*.*")))
+data = pd.read_csv(root.filename, skipfooter = 1, sep = "\t|,| |=|\)|\(|\{|\}|F.", header = None, engine = 'python')
 df = pd.DataFrame(data)
+# Get the row count
+with open(root.filename, "r") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    row_count = len(data)
+# Read only the final line for the accept states
+dataN = pd.read_csv(root.filename, skiprows = row_count - 1, sep = "\t|,| |=|\)|\(|\{|\}|F.", header = None, engine = 'python')
+dfN = pd.DataFrame(dataN)
+finales = []
+for i in range(len(dfN.iloc[0])):
+    if(not np.isnan(dfN.iloc[0][i])):
+        finales.append(dfN.iloc[0][i])
+
+print("@Author: Hector Ivan Garcia-Hernandez\n")
+e = input("Input the symbol to be used as 'epsilon': ")
 
 # Auxiliar array
 states = [[] for i in range(len(df))]
@@ -21,7 +38,7 @@ for row in range(len(df)):
             states[row].append(df.iloc[row][i])
 
 # Casting number of states into ints
-max = -1 # Variable to store the bigger node
+max = -1 # Variable to store the bigger numbered state
 for i in range(len(states) - 1):
     states[i][0] = int(states[i][0])
     states[i][2] = int(states[i][2])
@@ -30,14 +47,14 @@ for i in range(len(states) - 1):
     if (max < int(states[i][2])):
         max = int(states[i][2])
 
-for i in range(len(states[-1])):
-    states[-1][i] = int(states[-1][i])
+for i in range(len(finales)):
+    finales[i] = int(finales[i])
 
 # Adding epsilon transitions
-states = [[0, 'E', 1]] + states
-finales = states.pop(-1) # Retrieve the list of final nodes in the original FDA
+states = [[0, e, 1]] + states
+#finales = states.pop(-1) # Retrieve the list of final nodes in the original FDA
 for i in range(len(finales)):
-    states += [[finales[i], 'E', max + 1]] # Add epsilon transition for each final state
+    states += [[finales[i], e, max + 1]] # Add epsilon transition for each final state
 finales = [max + 1] # Update the list of final states
 
 # Transform python 2d array to numpy array for sorting by two columns
@@ -55,18 +72,13 @@ for i in range(len(qs)):
     qs[i][0] = int(qs[i][0])
     qs[i][2] = int(qs[i][2])
 
-#TO TEST WE SKIP FILE READING
-#max = 3
-#qs = [[0, 'E', 1], [1, 'a', 2], [1, 'b', 3], [2, 'a', 1], [2, 'b', 2], [2, 'E', 4], [3, 'b', 1], [3, 'a', 2], [3, 'E', 4]]
-print("Initial transitions states, after adding epsilon (E) transitions:")
+# Start the process
+print("Initial transitions states, after adding epsilon ",e," transitions:")
 print(np.matrix(qs))
 
 istates = [i for i in range(0, max + 1)]
 jstates = [i for i in range(1, max + 2)]
 statesQrip = [i for i in range(1, max + 1)]
-#print("States to rip:", statesQrip)
-#print("States to be i", istates)
-#print("States to be j", jstates)
 
 while len(statesQrip):
     print("\nStates to rip:", statesQrip)
@@ -76,9 +88,8 @@ while len(statesQrip):
     print("i-states:", istates)
     jstates.remove(qrip)
     print("j-states:", jstates)
-    #print("Current states transitions:")
-    #print(np.matrix(qs))
-    maux = []
+
+    maux = [] # Auxiliar matrix to store the temporarly regular expressions
     for i in istates:
         for j in jstates:
             regex = ""
@@ -99,48 +110,14 @@ while len(statesQrip):
             if (r1 != "" and r3 != ""):
                 if (r4 != ""):
                     if (r2 == ""):
-                        if (r1 != 'E'):
-                            if (r3 != 'E'):
-                                regex = "("+r1 + r3 + "|" + r4+")"
-                            else:
-                                regex = "("+r1 + "|" + r4+")"
-                        else:
-                            if (r3 != 'E'):
-                                regex = "("+r3 + "|" + r4+")"
-                            else:
-                                regex = r4 #Tal vez innecesario
+                        regex = "("+r1 + r3 + "|" + r4+")"
                     else:
-                        if (r1 != 'E'):
-                            if (r3 != 'E'):
-                                regex = "("+r1 + "(" + r2 + ")*" + r3 + "|" + r4+")"
-                            else:
-                                regex = "("+r1 + "(" + r2 + ")*" + "|" + r4+")"
-                        else:
-                            if (r3 != 'E'):
-                                regex = "("+"(" + r2 + ")*" + r3 + "|" + r4+")"
-                            else:
-                                regex = "("+"(" + r2 + ")*" + "|" + r4+")"
+                        regex = "("+r1 + "(" + r2 + ")*" + r3 + "|" + r4+")"
                 else:
                     if (r2 == ""):
-                        if (r1 != 'E'):
-                            if (r3 != 'E'):
-                                regex = r1 + r3
-                            else:
-                                regex = r1
-                        else:
-                            if (r3 != 'E'):
-                                regex = "("+r3+")"
+                        regex = r1 + r3
                     else:
-                        if (r1 != 'E'):
-                            if (r3 != 'E'):
-                                regex = r1 + "(" + r2 + ")*" + r3
-                            else:
-                                regex = r1 + "(" + r2 + ")*"
-                        else:
-                            if (r3 != 'E'):
-                                regex = "(" + r2 + ")*" + r3
-                            else:
-                                regex = "(" + r2 + ")*"
+                        regex = r1 + "(" + r2 + ")*" + r3
             else:
                 regex = r4
 
@@ -157,7 +134,7 @@ print("\nRegEx in standard form:")
 print(qs[-1][1])
 print("\nRegEx in \"web\" form:")
 print(qs[-1][1].replace("|", "+"))
-input("\Press enter to exit")
+input("Press enter to exit")
 
 
 
